988eba37729219cfc01d5ba3baae5459843f35b1
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-05-02 08:57:57 +0000

    Data driven test storing data in an array

--- src/test/java/com/example/demo1/dataProviderAnnotation.java
+++ src/test/java/com/example/demo1/dataProviderAnnotation.java
@@ -19,15 +19,17 @@ public class dataProviderAnnotation {
         driver.get("https://signup.ebay.com/pa/crte?usage=2943");
         driver.manage().window().maximize();
     }
-//    @DataProvider(name="userDetails")
-//    public Object[][]userDetails(){
-//        return new Object[][]{
-//
-//
-//        }
-//
-//    }
-    @Test
+    @DataProvider(name="userDetails")
+    public Object[][]userDetails(){
+        Object [][]data = new Object[3][4];
+        data[0][0]="Bertina "; data[0][1]="Ayuure "; data[0][2]=" bert@gmail.com"; data[0][3]=" 123@Astore";
+        data[1][0]="YT "; data[1][1]="htk "; data[1][2]="yt@gmail.com "; data[1][3]="123@Astore ";
+        data[2][0]=" wrwfwef"; data[2][1]=" grevx"; data[2][2]="hty@gmail.com "; data[2][3]="123@Astore ";
+
+        return data;
+
+    }
+    @Test(dataProvider = "userDetails")
     public void site(String firstname, String lastname, String email, String password){
         driver.findElement(By.id("firstname")).sendKeys(firstname);
         driver.findElement(By.id("lastname")).sendKeys(lastname);
--- src/test/java/com/example/demo1/test.java
+++ src/test/java/com/example/demo1/test.java
@@ -1,5 +1,45 @@
 package com.example.demo1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
+
 public class test {
+    //public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+//        WebDriverManager.chromedriver().setup();
+//        driver= new ChromeDriver();
+//        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//        driver.manage().window().maximize();
+//        Thread.sleep(3000);
+        System.out.println("Before method 1");
+       //Assert.assertEquals(true,false, "test failed");
+        System.out.println(" this is the next test under my before block");
+    }
+
+    @Test
+    public void login(){
+        System.out.println("User was able to log in");
+        SoftAssert soft = new SoftAssert();
+        soft.assertEquals(false,true, "passed");
+        System.out.println("This is under a failed test");
+        soft.assertAll();
+    }
+    @Test
+    public void logout(){
+        System.out.println("User is logged out");
+
+    }
+
+
+
+
 
 }
